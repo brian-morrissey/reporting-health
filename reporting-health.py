f5bf431d45d4b7133166152eb9dcdc68bc31a250
brian.morrissey <brian.morrissey@sysdig.com>
2024-12-09 08:48:42 -0500

    additions to check agent status

--- reporting-health.py
+++ reporting-health.py
@@ -55,7 +55,7 @@ def main():
 
         # Get clusters from runtime findings
         try:
-            url = f"https://{arg_secure_url_authority}/api/secure-metrics/v1/vm-cve/filter/cluster?from={h_24_epoch}&to={now_epoch}&filter=context+%3D+%22runtime%22&limit=500"
+            url = f"https://{arg_secure_url_authority}/api/secure-metrics/v1/vm-cve/filter/cluster?from={d_30_epoch}&to={now_epoch}&filter=context+%3D+%22runtime%22&limit=500"
             response = http_client.request(method="GET", url=url, redirect=True, timeout=3)
         except Exception as e:
             print(f"An error occurred: {e}")
@@ -73,14 +73,35 @@ def main():
                 except Exception as e:
                     print(f"An error occurred: {e}")
                     quit()
+
                 if response.status == 200:
                     json_response_data = json.loads(response.data.decode())
                     for result in json_response_data.get('results', []):
-                        for series in result.get('series', []):
-                            print(series["pointInTime"])
-                            print(series["value"])
-                            
-                            
+                        values = [point["value"] for point in result["series"]]
+                        if len(values) >= 5:
+                            last_5_values = values[-5:]
+                            max_value = max(values)
+                            if all(v == 0 or v < 0.1 * max_value for v in last_5_values):
+                                print(f"Cluster {cluster_name}: Last 5 daily scanning trend values are 0 or less than 10% of the maximum value.")     
+
+                                # Get Agent Status
+                                try:
+                                    url = f"https://{arg_secure_url_authority}/api/cloud/v2/dataSources/agents?limit=50&offset=0&filter={cluster_name}"
+                                    response = http_client.request(method="GET", url=url, redirect=True, timeout=3)
+                                except Exception as e:
+                                    print(f"An error occurred: {e}")
+                                    quit()
+                                    
+                                if response.status == 200:
+                                    json_agent_data = json.loads(response.data.decode())
+                                    for agent in json_agent_data['details']:
+                                        print(f"\tAgent Status: {agent['clusterName']} {agent['labels']['hostname']} {agent['agentStatus']}")
+                                else:
+                                   raise UnexpectedHTTPResponse(
+                                            f"Unexpected HTTP response status: {response.status}"
+                                        )
+
+
                 else:  
                     raise UnexpectedHTTPResponse(
                         f"Unexpected HTTP response status: {response.status}"
